61dff7a1afed748ae7dff4ec3d456740ad89393d
leonardo580 <anasbenbrahim9@gmail.com>
2024-04-03 13:45:39 +0000

    second

--- lib/bot_kb/hooks/bot_kb_view_hook.rb
+++ lib/bot_kb/hooks/bot_kb_view_hook.rb
@@ -1,12 +1,18 @@
 
 class BotKbViewHook < Redmine::Hook::ViewListener
   def view_issues_show_description_bottom(context = {})
-  puts "loaded"
+  issue = context[:issue]
+  Rails.cache.fetch("issue_#{issue.id}_button", expires_in: 1.hour) do
   return context[:controller].send(:render_to_string, {
     partial: "hooks/view_issue_button",
     locals: { issue: context[:issue] }
   })
 end
+end
+# def view_issues_show_description_bottom(context={})
+#     issue = context[:issue]
+#     content_tag :button, 'Open Modal', class: 'btn btn-primary', data: { toggle: 'modal', target: '#modal-form' }
+#   end
   
    render_on :view_issues_index_top, partial: "views/hooks/view_issue_button"
   end
